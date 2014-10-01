18421d6656422098ce4510641b431f62e62eaad9
Franck Silvestre <franck.silvestre@ticetime.com>
2014-10-01 13:16:23 +0200

    écriture de tests avec Mock

--- src/test/groovy/manm2dl/ProductSpec.groovy
+++ src/test/groovy/manm2dl/ProductSpec.groovy
@@ -7,25 +7,48 @@ import spock.lang.Specification
  */
 class ProductSpec extends Specification{
 
+    Product produit
 
-    def "test recuperation des options ordonnees par nom quand la liste"() {
+    def setup() {
+        // construction de l'objet à tester avec ses collaborateurs
+        def options = [Mock(Option),
+                       Mock(Option),
+                       Mock(Option)]
+        produit = new Product(name: "p1",
+                price: 20,
+                optionList: options)
 
-        given:"une liste d'options"
-        def options = [new Option(name:"z_opt"),
-                       new Option(name:  "a_opt"),
-                       new Option(name: "l_opt")]
+    }
 
-        and: "un produit pointant sur la liste d'options"
-        def produit = new Product(optionList: options)
+    def "test recuperation des options ordonnees par nom"() {
+
+        given:"une liste d'options non ordonnée"
+        produit.optionList[0].getName() >> "z_opt"
+        produit.optionList[1].getName() >> "a_opt"
+        produit.optionList[2].getName() >> "l_opt"
 
         when:" la liste ordonnée par nom est renvoyée"
         def res = produit.optionListOrderedByName()
 
         then: "la liste est correctement ordonnée"
-        res == options.sort { it.name }
+        res == produit.optionList.sort { it.name }
 
     }
 
+    def "test recuperation des options ordonnees par prix"() {
+
+        given:"une liste d'options non ordonnée"
+        produit.optionList[0].getPrice() >> "8"
+        produit.optionList[1].getPrice() >> "12"
+        produit.optionList[2].getPrice() >> "4"
+
+        when:" la liste ordonnée par nom est renvoyée"
+        def res = produit.optionListOrderedByPrice()
+
+        then: "la liste est correctement ordonnée"
+        res == produit.optionList.sort { it.price }
+
+    }
 
 
 }
